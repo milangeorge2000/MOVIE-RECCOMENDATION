f8b5269b6fdbe24d82a6b6b8c9ad3aa7ca85aa5f
milangeorge2000 <45516866+milangeorge2000@users.noreply.github.com>
2024-12-29 06:01:44 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './template/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
